24a1c300edc6ce9b6c465a72a4804ac22ac34acd
Angus <131868779+vaiogtx@users.noreply.github.com>
2023-06-29 16:52:57 +0700

    Add static pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,12 +18,14 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    return render(request, "djangoapp/about.html")
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    return render(request, "djangoapp/contact.html")
+
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
@@ -43,7 +45,6 @@ def get_dealerships(request):
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
-
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
@@ -51,4 +52,3 @@ def get_dealerships(request):
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
-
